2f4379ab36092b9b2e46e9729d803b5cde3d802c
roshi2602 <roshidubey2602@gmail.com>
2021-06-16 18:56:24 +0530

    first commit

---
+++ Extension method class library/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Myclass
+    {
+        public string Display1()
+        {
+            return ("display1");  //console.writeline will not work here
+        }
+
+        public string Display2()
+        {
+            return ("display2");  //console.writeline will not work here
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using ClassLibrary;
+
+//class library is made for this
+//extension methods allows additional methods without modifying the original class
+//IsGreaterThan() is an extension method for int type
+//An extension method is actually a special kind of static method defined in a static class.
+//To define an extension method, first of all, define a static class.
+//this - is used in it
+namespace ExtensionMethod
+{
+    //create 1 static class
+    public static class Lib
+    {
+        //create static method
+
+        public static void NewMethod(this Myclass ob)  //using this keyword to extend my class library and adding it to my method
+        {
+            Console.WriteLine("this is extension method");
+        }
+    }
+
+        class Program
+        {
+
+            static void Main()
+            {
+            //creating instance and calling
+                Myclass ob = new Myclass();
+                ob.Display1();
+                ob.Display2();
+                ob.NewMethod();
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+        
+   
+
